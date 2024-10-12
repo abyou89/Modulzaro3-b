e3171fe6493696ffac21b2d04a952951342ebd28
younes <younes.abdalla@gmail.com>
2024-10-12 10:33:07 +0200

    Elso feladat kesz

--- src/main/java/hu/younes/Main.java
+++ src/main/java/hu/younes/Main.java
@@ -1,7 +1,58 @@
 package hu.younes;
 
+import hu.younes.modell.Ember;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws IOException {
+        new Main().feladatok();
+    }
+
+    private void feladatok() throws IOException {
+        InputStream is = Main.class.getClassLoader().getResourceAsStream("emberek.csv");
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String sor;
+        List<Ember> emberek = new ArrayList<>();
+        boolean fejlec = true;
+
+        while ((sor = br.readLine()) != null) {
+            if (fejlec) {
+                fejlec = false;
+                continue;
+            }
+            String[] adatok = sor.split(";");
+            if (adatok.length >= 3) {
+                String nev = adatok[0];
+                int kor = Integer.parseInt(adatok[1]);
+
+
+                List<String> nyelvek = new ArrayList<>();
+                for (int i = 2; i < adatok.length; i++) {
+                    nyelvek.add(adatok[i]);
+                }
+
+                Ember ember = new Ember(nev, kor, nyelvek);
+                emberek.add(ember);
+            }
+        }
+
+        for (Ember ember : emberek) {
+            System.out.println(ember);
+        }
     }
 }
+
+
+
+
+
+    
+
+    
--- src/main/java/hu/younes/modell/Ember.java
+++ src/main/java/hu/younes/modell/Ember.java
@@ -1,7 +1,27 @@
 package hu.younes.modell;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ember {
 
-    
+    private String nev;
+    private int kor;
+    private List<String> nyelvTudas;
 
+    public Ember(String nev, int kor, List<String> nyelvTudas) {
+        this.nev = nev;
+        this.kor = kor;
+        this.nyelvTudas = new ArrayList<>(nyelvTudas);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Ember{" +
+                "nev='" + nev + '\'' +
+                ", kor=" + kor +
+                ", nyelvTudas='" + nyelvTudas + '\'' +
+                '}';
+    }
 }
